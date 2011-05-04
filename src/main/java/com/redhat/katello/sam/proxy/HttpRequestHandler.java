a644c89225ab4dce62baac4d9c0c7cac9af8ba15
James Bowes <jbowes@redhat.com>
2011-05-04 15:29:30 -0300

    Checkstyle fixes

--- src/main/java/com/redhat/katello/sam/proxy/HttpRequestHandler.java
+++ src/main/java/com/redhat/katello/sam/proxy/HttpRequestHandler.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2011 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
 package com.redhat.katello.sam.proxy;
 
 import static org.jboss.netty.handler.codec.http.HttpHeaders.*;
@@ -33,6 +47,9 @@ import org.jboss.netty.handler.codec.http.HttpResponse;
 import org.jboss.netty.handler.codec.http.QueryStringDecoder;
 import org.jboss.netty.util.CharsetUtil;
 
+/**
+ * HttpRequestHandler
+ */
 public class HttpRequestHandler extends SimpleChannelUpstreamHandler {
 
     private HttpRequest request;
--- src/main/java/com/redhat/katello/sam/proxy/HttpServerPipelineFactory.java
+++ src/main/java/com/redhat/katello/sam/proxy/HttpServerPipelineFactory.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2011 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
 package com.redhat.katello.sam.proxy;
 
 import static org.jboss.netty.channel.Channels.*;
@@ -8,6 +22,9 @@ import org.jboss.netty.handler.codec.http.HttpContentCompressor;
 import org.jboss.netty.handler.codec.http.HttpRequestDecoder;
 import org.jboss.netty.handler.codec.http.HttpResponseEncoder;
 
+/**
+ * HttpServerPipelineFactory
+ */
 public class HttpServerPipelineFactory implements ChannelPipelineFactory {
 
     public ChannelPipeline getPipeline() throws Exception {
