9f20575f4d9480694ad27862a05da491ea6314cb
Chris Duryee (beav) <cduryee@redhat.com>
2011-06-23 14:42:07 -0400

    have a mock http server during spec tests

--- spec/proxy_spec.rb
+++ spec/proxy_spec.rb
@@ -1,6 +1,30 @@
-#require 'candlepin_scenarios'
+require 'webrick'
+include WEBrick
 
-describe 'Something' do
+describe 'HTTP proxying' do
+
+  before(:all) do
+    config = {
+      :Port => 9090,
+      :BindAddress => '127.0.0.1',
+      :DocumentRoot => Dir.pwd + '/spec/data/',
+      #comment out these two lines to enable webrick logging
+      :Logger => WEBrick::Log.new("/dev/null"),
+      :AccessLog => [nil, nil],
+    }
+    
+    @http_proc = fork {
+      server = HTTPServer.new(config)
+      trap('INT') { server.stop }
+      server.start
+    }
+  end
+
+  after(:all) do
+    #do any cleanup
+    Process.kill('INT', @http_proc)
+  end
+
 
   it 'should do stuff' do
     1.should == 1
