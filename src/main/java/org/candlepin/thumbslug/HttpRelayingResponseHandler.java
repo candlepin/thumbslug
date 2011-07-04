e36081e6827b6fdf669bfeb976ec49b767b90138
James Bowes <jbowes@redhat.com>
2011-07-04 16:47:42 -0300

    Fix IllegalStateException when sending bad gateway

--- src/main/java/org/candlepin/thumbslug/HttpRelayingResponseHandler.java
+++ src/main/java/org/candlepin/thumbslug/HttpRelayingResponseHandler.java
@@ -16,6 +16,7 @@ package org.candlepin.thumbslug;
 
 import org.apache.log4j.Logger;
 import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.ChannelEvent;
 import org.jboss.netty.channel.ChannelFuture;
 import org.jboss.netty.channel.ChannelFutureListener;
 import org.jboss.netty.channel.ChannelHandlerContext;
@@ -38,33 +39,26 @@ public class HttpRelayingResponseHandler extends SimpleChannelUpstreamHandler {
     private boolean readingChunks;
     private Channel client;
     private boolean keepAlive;
+    private boolean pendingException;
 
     public HttpRelayingResponseHandler(Channel client, boolean keepAlive) {
         this.client = client;
         this.keepAlive = false;
+        this.pendingException = false;
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e)
         throws Exception {
-        // if we aren't reading bits yet and we run into an issue,
-        // fire off a 502 to the client
-        if (!readingChunks) {
-            
-            HttpResponse response = new DefaultHttpResponse(HttpVersion.HTTP_1_1,
-                HttpResponseStatus.BAD_GATEWAY);
-            ChannelFuture future = client.write(response);
-            future.addListener(new ChannelFutureListener() {
-                @Override
-                public void operationComplete(ChannelFuture arg0) throws Exception {
-                    client.close();
-                }
-            });
+        /*
+         * We want to send a 502 bad_gateway to the client here, but most likely this is
+         * happening because of a misconfiguration, and we've timedout trying to connect to
+         * the cdn. Thus, the client hasn't even had a chance yet to send its request up.
+         * Delay sending the 502 till after the client begins a write, then shut them down.
+         */ 
+        pendingException = true;
 
-        }
-
-        e.getCause().printStackTrace();
-        client.close(); //if something happens, close the connection
+        //e.getCause().printStackTrace();
     }
     /**
      * This is an event *to* the client coming *from* the cdn
@@ -113,6 +107,31 @@ public class HttpRelayingResponseHandler extends SimpleChannelUpstreamHandler {
     }
     
     
+    /**
+     * A request going *from* the client *to* the cdn.
+     * At the moment we only intercept these in this class to send back a bad gateway
+     * reply if needed.
+     * 
+     * @throws Exception - an exception
+     */
+    @Override
+    public void handleUpstream(ChannelHandlerContext ctx, ChannelEvent e)
+        throws Exception {
+        if (pendingException) {
+            HttpResponse response = new DefaultHttpResponse(HttpVersion.HTTP_1_1,
+                HttpResponseStatus.BAD_GATEWAY);
+            ChannelFuture future = client.write(response);
+            if (!keepAlive) {
+                future.addListener(new ChannelFutureListener() {
+                    @Override
+                    public void operationComplete(ChannelFuture arg0) throws Exception {
+                        client.close();
+                    }
+                });
+            }
+        }
+        super.handleUpstream(ctx, e);
+    }
     
 //    private void writeResponse(MessageEvent e) {
 //        // Decide whether to close the connection or not.
