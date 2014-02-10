5943fcb204c2a8e1b3d9426fd171fa4ec6e8f713
Alex Wood <awood@redhat.com>
2014-02-10 12:26:26 -0500

    Update thumbslug shell script to use jpackage-utils
    This script is now very similar to what the %jpackage_script RPM
    macro produces.

---
+++ thumbslug.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+if [ ! -r /usr/share/java-utils/java-functions ]; then
+    echo "Please 'yum install jpackage-utils'"
+    exit 1
+fi
+
+_prefer_jre="true"
+. /usr/share/java-utils/java-functions
+
+# The readlink trick gets us the name of the directory this script is in
+CLASSPATH="$(dirname $(readlink -f $0))/target/classes"
+
+# Configuration
+MAIN_CLASS=org.candlepin.thumbslug.Main
+BASE_FLAGS=""
+BASE_OPTIONS=""
+BASE_JARS="netty log4j jna commons-codec akuma oauth oauth-consumer"
+
+# Set parameters
+set_jvm
+set_classpath $BASE_JARS
+set_flags $BASE_FLAGS
+set_options $BASE_OPTIONS
+
+# Let's start
+run "$@"
