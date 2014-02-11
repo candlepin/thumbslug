0d3dd473483ce7ebf7723e6ac4b9b6d597e3c788
Alex Wood <awood@redhat.com>
2014-02-11 09:45:02 -0500

    Add resources to classpath for thumbslug script.

--- thumbslug.sh
+++ thumbslug.sh
@@ -9,7 +9,8 @@ _prefer_jre="true"
 . /usr/share/java-utils/java-functions
 
 # The readlink trick gets us the name of the directory this script is in
-CLASSPATH="$(dirname $(readlink -f $0))/target/classes"
+PROJECT_DIR="$(dirname $(readlink -f $0))"
+CLASSPATH="${PROJECT_DIR}/target/classes:${PROJECT_DIR}/target/resources"
 
 # Configuration
 MAIN_CLASS=org.candlepin.thumbslug.Main
